c70f9b3b508df9eff18aafd633b71c493a215247
ivanberezin <ivanberezin@ukr.net>
2021-05-20 17:07:08 +0300

    2021-05-20

---
+++ api/contacts/contact.router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ContactController = require("./contact.controller");
+
+const contactRouter = express.Router();
+
+contactRouter.get("/", ContactController.listContacts);
+
+contactRouter.get("/:id", ContactController.getById);
+
+contactRouter.post("/", ContactController.validateCreateContact, ContactController.addContact);
+
+contactRouter.put("/:id", ContactController.validateUpdateContact, ContactController.updateContact);
+
+contactRouter.delete("/:id", ContactController.removeContact);
+
+// console.log("contactRouter", contactRouter);
+
+module.exports = contactRouter;
